555b8e1fcc286b77605bbc8e9cdfc6c8e6389b01
Armando6464 <armando95_95@hotmail.com>
2017-01-13 04:37:33 +0100

    Realizada modificación de comentario en rama development

--- src/main/Sum.java
+++ src/main/Sum.java
@@ -7,7 +7,7 @@ public class Sum {
 		return a + b;
 	}
 	
-	//Suma de tres números
+	//Suma de tres números (escrito en development)
 	public static int sum(int a, int b, int c){
 		return sum(a,b) + c;
 	}
