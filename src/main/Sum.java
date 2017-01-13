71edde77333ed953f39a989438d00b0369438e35
Armando6464 <armando95_95@hotmail.com>
2017-01-13 04:35:50 +0100

    Realizada modificación de comentario en rama development

--- src/main/Sum.java
+++ src/main/Sum.java
@@ -2,12 +2,12 @@ package main;
 
 public class Sum {
 	
-	//Suma de dos números (escrito en master)
+	//Suma de dos números
 	public static int sum(int a, int b){
 		return a + b;
 	}
 	
-	//Suma de tres números
+	//Suma de tres números (escrito en development)
 	public static int sum(int a, int b, int c){
 		return sum(a,b) + c;
 	}
