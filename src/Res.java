d044f9ac9ea0d4c694cfe154e513e9b5f7adffe1
Armando6464 <armando95_95@hotmail.com>
2017-01-13 03:32:14 +0100

    Añadido método en Res para restar tres números

--- src/Res.java
+++ src/Res.java
@@ -2,8 +2,12 @@
 public class Res {
 	
 	//Resta de dos números
-	public static int sum(int a, int b){
+	public static int res(int a, int b){
 		return a + b;
 	}
 
+	//Resta de tres números
+	public static int res(int a, int b, int c){
+		return res(a,b) + c;
+	}
 }
