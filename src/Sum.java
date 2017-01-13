0cb24eac3095141f131fa46dcb8714c6aa6f84c7
Armando6464 <armando95_95@hotmail.com>
2017-01-13 03:28:34 +0100

    Añadido método para sumar tres números

--- src/Sum.java
+++ src/Sum.java
@@ -5,5 +5,10 @@ public class Sum {
 	public static int sum(int a, int b){
 		return a + b;
 	}
+	
+	//Suma de tres números
+	public static int sum(int a, int b, int c){
+		return sum(a,b) + c;
+	}
 
 }
